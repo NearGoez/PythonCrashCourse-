5e1704400b34d9527c26fd8c98549d31295653cc
unfazedd <104703091+NearGoez@users.noreply.github.com>
2023-02-23 02:27:58 -0300

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,12 @@
+places = ["Concepcion", "Valdivia", "Santiago", "Talca", "Pichilemu"]
+print(sorted(places))
+print(sorted(places, reverse=True))
+print(places)
+places.reverse()
+print(places)
+places.reverse()
+print(places)
+places.sort()
+print(places)
+places.sort(reverse=True)
+print(places)
